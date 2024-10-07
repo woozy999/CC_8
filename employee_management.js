708283de510ba13d260153c1e013c82fffafb13a
k <kyle.offenbacher@gmail.com>
2024-10-06 21:20:14 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -16,3 +16,21 @@ class Employee {
     }
 }
 
+//task 2
+
+
+//make department class & add employee
+class Department {
+    constructor(name) {
+        this.name = name;
+        this.employees = [];
+    }
+
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+
+    getDepartmentSalary() {
+        return this.employees.reduce((total, employee) => total + employee.salary, 0);
+    }
+}
